4e53eb2addbde70ea823b1d8f73369ecbe710d35
Victor C. C. França <you@example.com>
2021-03-02 17:46:16 -0300

    add arquivo do jenkinsfile

--- src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
+++ src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
@@ -2,9 +2,12 @@ package br.ce.wcaquino.taskbackend.utils;
 
 import java.time.LocalDate;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class DateUtils {
 
 	public static boolean isEqualOrFutureDate(LocalDate date) {
 		return date.isEqual(LocalDate.now()) || date.isAfter(LocalDate.now());
+	
 	}
 }
